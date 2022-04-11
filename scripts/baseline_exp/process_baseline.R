34585d971e1645e3ea4588916939e8da3bed15fd
pracz_station <petermartonracz@gmail.com>
2022-04-11 19:46:23 +0200

    some analyses

---
+++ scripts/baseline_exp/process_baseline.R
@@ -0,0 +1,108 @@
+setwd('~/Github/Racz2024/')
+
+library(tidyverse)
+library(glue)
+library(magrittr)
+library(lubridate)
+
+col_specs = cols(
+  my_list = col_character(),
+  key.keys = col_character(),
+  key.rt = col_double(),
+  key_2.keys = col_character(),
+  key_2.rt = col_double(),
+  keyboard_input_2.keys = col_character(),
+  keyboard_input_2.rt = col_double(),
+  key_3.keys = col_character(),
+  key_3.rt = col_double(),
+  keyboard_input_3.keys = col_character(),
+  keyboard_input_3.rt = col_double(),
+  key_4.keys = col_character(),
+  key_4.rt = col_double(),
+  keyboard_input_4.keys = col_character(),
+  keyboard_input_4.rt = col_double(),
+  key_5.keys = col_character(),
+  key_5.rt = col_double(),
+  my_button1 = col_character(),
+  my_button2 = col_character(),
+  keyboard_input.keys = col_character(),
+  keyboard_input.rt = col_double(),
+  withinBlock.thisRepN = col_double(),
+  withinBlock.thisTrialN = col_double(),
+  withinBlock.thisN = col_double(),
+  withinBlock.thisIndex = col_double(),
+  withinBlock.ran = col_double(),
+  rowname = col_double(),
+  word = col_character(),
+  prompt = col_character(),
+  suffix = col_character(),
+  vowel = col_character(),
+  carrier_sentence = col_character(),
+  target_sentence = col_character(),
+  target1 = col_character(),
+  target2 = col_character(),
+  category = col_character(),
+  derivational = col_character(),
+  nsyl = col_double(),
+  Azonosító = col_character(),
+  `Melyik évben születtél?` = col_double(),
+  `Mi a nemed?` = col_character(),
+  `Tanultál nyelvészetet?` = col_character(),
+  date = col_character(),
+  expName = col_character(),
+  psychopyVersion = col_character(),
+  OS = col_character(),
+  frameRate = col_double()
+)
+
+file_name = list.files('exp_data/baseline/data/') %>% 
+  str_subset('.*csv') %>% 
+  str_subset('hesp')
+
+d = tibble(
+  file_name
+) %>% 
+  mutate(
+    file_name_date = str_extract(file_name, '2022-..-..') %>% 
+      ymd()
+  ) %>% 
+  filter(file_name_date >= '2022-03-25')
+
+d %<>%
+  mutate(
+    dat = map(file_name, ~ read_csv(
+      glue('exp_data/baseline/data/{.}'),
+      col_types = col_specs
+      ))
+  )
+
+d %<>% 
+  unnest(cols = dat)
+
+d %>% 
+  filter(!is.na(my_list)) %>% 
+  distinct(file_name,my_list,Azonosító)
+
+d %>% 
+  filter(!is.na(my_list)) %>% 
+  count(my_list)
+
+d %>% 
+  count(Azonosító)
+
+d %<>%
+  fill(my_list) %>% 
+  mutate(
+    my_choice = case_when(
+      keyboard_input.keys == 'left' ~ my_button1,
+      keyboard_input.keys == 'right' ~ my_button2
+    ),
+    resp_is_first_variant = case_when(
+      my_choice == target1 ~ T,
+      my_choice == target2 ~ F
+    ) 
+  )
+
+d %>% 
+  select(file_name,file_name_date,my_list,my_button1,my_button2,keyboard_input.keys,keyboard_input.rt,withinBlock.thisN,word,prompt,suffix,vowel,carrier_sentence,target_sentence,target1,target2,my_choice,resp_is_first_variant,category,derivational,nsyl,Azonosító,`Melyik évben születtél?`,`Mi a nemed?`,`Tanultál nyelvészetet?`,date,expName,psychopyVersion,OS) %>% 
+  write_tsv('exp_data/baseline/baseline_tidy.tsv')
--- scripts/build_words/build_nonce_list/ultimate_filter_and_sort.R
+++ scripts/build_words/build_nonce_list/ultimate_filter_and_sort.R
@@ -1,3 +1,5 @@
+# load filtered samples, write them into a google sheet, filter them by hand, load them back in here, split them across input lists, write into exp dir
+
 # -- header -- #
 
 setwd('~/Github/Racz2024')
@@ -7,6 +9,7 @@ set.seed(1989)
 library(tidyverse)
 library(glue)
 
+"
 # -- read -- #
 
 dzsungel = read_tsv('resource/nonce_words/nouns_filt_sample.tsv')
@@ -65,7 +68,7 @@ cselekszek3 = cselekszek2 %>%
 # count(cselekszek3,nsyl,suffix)
   
 # -- write -- #
-"
+
 dzsungel3 %>% 
   googlesheets4::write_sheet(ss = 'https://docs.google.com/spreadsheets/d/1Iximhc57X1yYPwtm10ubvYfNaT7YL7jje5RkOilUAPw/edit?usp=sharing', sheet = 'dzsungelban/dzsungelben')
 
@@ -76,80 +79,156 @@ cselekszek3 %>%
   googlesheets4::write_sheet(ss = 'https://docs.google.com/spreadsheets/d/1Iximhc57X1yYPwtm10ubvYfNaT7YL7jje5RkOilUAPw/edit?usp=sharing', sheet = 'cselekszik/cselekedik')
 "
 
-# -- trial tsv -- #
+# -- one filtering later -- #
 
-sample1 = cselekszek %>% 
-  filter(keep_enough_distance,keep_no_overlap) %>% 
-  sample_n(3) %>% 
-  rename(
-    prompt = prompt_cc,
-    target1 = target_cc,
-    target2 = target_cvc,
-    category = suffix_ep,
-    syllables = nsyl
-  ) %>% 
-  mutate(
-    type = 'ep',
-    target_sentence = case_when(
-      category == 'na' ~ 'Ő holnap is boldogan...',
-      category == 'nak' ~ 'A barátai is sokat...',
-      category == 'ni' ~ 'Én is szeretnék...',
-      category == 'tak' ~ 'Régen a barátai sokat...',
-      category == 'tok' ~ 'Ti is sokat...'
-    ),
-    carrier_sentence = glue('János sokat {prompt}.')
-  ) %>% 
-  select(prompt,carrier_sentence,target_sentence,target1,target2,category,syllables) 
+dzsungel =
+  googlesheets4::read_sheet(ss = 'https://docs.google.com/spreadsheets/d/1Iximhc57X1yYPwtm10ubvYfNaT7YL7jje5RkOilUAPw/edit?usp=sharing', sheet = 'dzsungelban/dzsungelben')
 
-sample2 = dzsungel %>% 
-  filter(keep_enough_distance,keep_no_overlap) %>% 
-  sample_n(3) %>% 
-  rename(
-    # prompt = prompt_cc,
-    target1 = target_front,
-    target2 = target_back,
-    category = suffix_back
-    # syllables = nsyl
-  ) %>% 
+eszek = 
+  googlesheets4::read_sheet(ss = 'https://docs.google.com/spreadsheets/d/1Iximhc57X1yYPwtm10ubvYfNaT7YL7jje5RkOilUAPw/edit?usp=sharing', sheet = 'lakok/lakom')
+
+cselekszek = 
+  googlesheets4::read_sheet(ss = 'https://docs.google.com/spreadsheets/d/1Iximhc57X1yYPwtm10ubvYfNaT7YL7jje5RkOilUAPw/edit?usp=sharing', sheet = 'cselekszik/cselekedik')
+
+# -- filtering -- #
+
+list_length = 162
+
+dzsungel2 = dzsungel %>% 
+  filter(is.na(Petya),is.na(Ági)) %>% 
+  group_by(vowel) %>% 
+  sample_n(list_length / 2) %>% 
+  ungroup() %>% 
+  select(prompt,vowel,ban,nak,nál) %>% 
+  sample_n(n()) %>% 
+  arrange(vowel) %>% 
+  mutate(keep = rep(c('ban','nak','nál'),list_length/3)) %>% 
+  pivot_longer(-c(prompt,vowel,keep), names_to = 'suffix') %>% 
+  filter(keep == suffix)
+
+dzsungel3 = dzsungel2 %>% 
   mutate(
-    type = 'noun',
+    word = prompt,
+    target1 = str_extract(value, '^.*(?= /)'),
+    target2 = str_extract(value, '(?<=/ ).*$'),
+    category = 'dzsungel',
     target_sentence = case_when(
-      category == 'ba' ~ 'Bemegyünk a...',
-      category == 'ban' ~ 'Benne vagyunk a...',
-      category == 'nak' ~ 'Odaadom a csomagot a...',
-      category == 'nál' ~ 'Itt vagyunk a...',
-      category == 'ra' ~ 'Ráteszem a dobozt a...'
+      suffix == 'ban' ~ 'Ott vagyunk a...',
+      suffix == 'nak' ~ 'Elneveztem a kutyámat...',
+      suffix == 'nál' ~ 'Nincs is jobb egy jó...'
     ),
     carrier_sentence = glue('Ez itt egy {prompt}.')
   )%>% 
-  select(prompt,carrier_sentence,target_sentence,target1,target2,category) 
+  select(word,prompt,suffix,vowel,carrier_sentence,target_sentence,target1,target2,category) %>%
+  sample_n(n()) %>% 
+  rownames_to_column()
 
-sample3 = eszek %>% 
-  filter(keep_enough_distance,keep_no_overlap) %>% 
-  sample_n(3) %>% 
+count(dzsungel3, vowel, suffix)
+
+dzsungel3
+
+eszek2 = eszek %>% 
+  filter(is.na(Petya),is.na(Ági)) %>% 
+  group_by(nsyl) %>% 
+  sample_n(list_length / 2) %>% 
+  ungroup() %>% 
+  select(prompt,nsyl,suffix,target) %>% 
   rename(
-    # prompt = prompt_cc,
-    target1 = target_k,
-    target2 = target_m,
-    # category = suffix_back
-    syllables = nsyl
-  ) %>% 
+    word = prompt,
+    derivational = suffix
+    )
+
+eszek3 = eszek2 %>% 
   mutate(
-    type = 'ik',
-    carrier_sentence = glue('János sokat {prompt}.'),
-    target_sentence = 'Én is sokat...'
+    target1 = str_extract(target, '^.*(?= /)'),
+    target2 = str_extract(target, '(?<=/ ).*$'),
+    v = str_extract(target1, '.(?=k$)'),
+    v2 = case_when(
+      str_detect(v, '[ao]') ~ 'a',
+      str_detect(v, '[eö]') ~ 'e'
+                   ),
+    prompt = ifelse(derivational == '-lik',
+                    str_replace(word, 'ik$', glue('{v2}sz')),
+                    str_replace(word, 'ik$', glue('{v}l'))
+      ),
+    category = 'lakik',
+    target_sentence = 'Én is sokat...',
+    carrier_sentence = glue('Te bizony sokat {prompt}.')
   ) %>% 
-  select(prompt,carrier_sentence,target_sentence,target1,target2,syllables) 
+  select(word,prompt,derivational,nsyl,carrier_sentence,target_sentence,target1,target2,category) %>% 
+  sample_n(n()) %>% 
+  rownames_to_column()
 
-# sample = bind_rows(sample1,sample2,sample3) %>% 
-  # mutate_all( ~replace(., is.na(.), ''))
+count(eszek3, derivational, nsyl) # this will always be a crapshoot for the endings
+eszek3
 
-write_csv(sample1, 'exp/baseline/sample1.csv')
-write_csv(sample2, 'exp/baseline/sample2.csv')
-write_csv(sample3, 'exp/baseline/sample3.csv')
+cselekszek2 = cselekszek %>% 
+  filter(is.na(Petya),is.na(Ági)) %>% 
+  rename(word = prompt) %>% 
+  group_by(nsyl) %>% 
+  sample_n(list_length / 2) %>% 
+  ungroup() %>%  
+  select(word,nsyl,suffix,nek,tek,ünk) %>% 
+  rename(derivational = suffix) %>% 
+  sample_n(n()) %>% 
+  arrange(nsyl,derivational) %>%
+  mutate(
+    cc_prompt = str_extract(word, '^.*(?= /)'),
+    cvc_prompt = str_extract(nek, '(?<=/ ).*$'),
+    cvc_prompt = str_replace(cvc_prompt, '.k$', 'i'),
+    keep = rep(c('nek','tek','ünk'),list_length/3)
+    ) %>% 
+  pivot_longer(-c(word,cc_prompt,cvc_prompt,nsyl,derivational,keep), names_to = 'suffix') %>% 
+  filter(keep == suffix)
 
-master = tibble(
-  condsFile = c('sample1.csv','sample2.csv','sample3.csv')
+cselekszek3 = cselekszek2 %>% 
+  mutate(
+    prompt = glue('{cc_prompt}, {cvc_prompt}'),
+    target1 = str_extract(value, '^.*(?= /)'),
+    target2 = str_extract(value, '(?<=/ ).*$'),
+    category = 'cselekszik',
+    target_sentence = case_when(
+      suffix == 'nek' ~ 'Ők ritkán...',
+      suffix == 'tek' ~ 'Ti ritkán...',
+      suffix == 'ünk' ~ 'Mi ritkán...'
+    ),
+    carrier_sentence = glue('János imád {cvc_prompt}, ezért sokat {cc_prompt}.')
+  ) %>% 
+  select(word,prompt,suffix,nsyl,derivational,carrier_sentence,target_sentence,target1,target2,category) %>% 
+  sample_n(n()) %>% 
+  rownames_to_column()
+
+count(cselekszek3, nsyl, derivational, suffix)
+count(cselekszek3, nsyl, suffix)
+
+# -- sample into participant lists -- #
+
+list1 = bind_rows(
+  filter(dzsungel3, rowname %in% 1:(list_length/3)),
+  filter(eszek3, rowname %in% 1:(list_length/3)),
+  filter(cselekszek3, rowname %in% 1:(list_length/3))
 )
 
-write_csv(master, 'exp/baseline/master.csv')
+list2 = bind_rows(
+  filter(dzsungel3, rowname %in% (list_length/3+1):(list_length/3*2)),
+  filter(eszek3, rowname %in% (list_length/3+1):(list_length/3*2)),
+  filter(cselekszek3, rowname %in% (list_length/3+1):(list_length/3*2))
+)
+
+list3 = bind_rows(
+  filter(dzsungel3, rowname %in% (list_length/3*2+1):list_length),
+  filter(eszek3, rowname %in% (list_length/3*2+1):list_length),
+  filter(cselekszek3, rowname %in% (list_length/3*2+1):list_length)
+)
+
+count(list1, category, suffix, nsyl, vowel)
+count(list2, category, suffix, nsyl, vowel)
+count(list3, category, suffix, nsyl, vowel)
+
+# mindegy jó lesz
+
+# -- trial tsv -- #
+
+write_csv(list1, '~/Github/Pavlovia/hesp_baseline/list1.csv')
+write_csv(list2, '~/Github/Pavlovia/hesp_baseline/list2.csv')
+write_csv(list3, '~/Github/Pavlovia/hesp_baseline/list3.csv')
