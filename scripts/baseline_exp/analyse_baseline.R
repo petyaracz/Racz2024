f14dacad896d38301faafbdc85959137ba2aa587
pracz_station <petermartonracz@gmail.com>
2022-04-19 22:42:06 +0200

    indiv var looks good

--- scripts/baseline_exp/analyse_baseline.R
+++ scripts/baseline_exp/analyse_baseline.R
@@ -14,6 +14,7 @@ library(tidyverse)
 library(gghalves)
 library(ggthemes)
 library(patchwork)
+library(googlesheets4)
 
 set.seed(1337)
 
@@ -43,7 +44,8 @@ BoxViolin = function(d,ez,az){
 
 # -- read -- #
 
-r2 = read_tsv('exp_data/baseline/baseline_tidy_sum.tsv')
+r = read_tsv('exp_data/baseline/baseline_tidy.tsv')
+r2 = read_tsv('exp_data/baseline/baseline_tidy_proc.tsv')
 s = read_tsv('resource/real_words/all_pairs_webcorpus2.tsv')
 seg = read_csv('resource/segment_strength.csv')
 
@@ -144,6 +146,42 @@ p7 = d %>%
   BoxViolin(derivational,type) +
   ggtitle('base ending')
 
+# -- people -- #
+
+r %>% 
+  distinct(Azonosító,file_name_date) %>% 
+  ggplot(aes(file_name_date)) +
+  geom_histogram()
+  
+r %>% 
+  count(Azonosító,resp_is_first_variant) %>% 
+  pivot_wider(names_from = resp_is_first_variant, values_from = n) %>% 
+  mutate(
+    log_odds =  log(`TRUE` / `FALSE`),
+    id = fct_reorder(Azonosító,log_odds)
+         ) %>% View
+  ggplot(aes(log_odds)) +
+  geom_histogram()
+  
+rpcat = r %>% 
+    count(Azonosító,category,resp_is_first_variant) %>% 
+    pivot_wider(names_from = resp_is_first_variant, values_from = n, values_fill = 0) %>% 
+    mutate(
+      log_odds =  log((`TRUE`+1)/(`FALSE`+1)),
+      id = fct_reorder(Azonosító,log_odds)
+    ) 
+
+rpcat %>% 
+  ggplot(aes(log_odds)) +
+    geom_histogram() +
+    facet_wrap(~ category)
+
+rpcat %>% 
+  ggplot(aes(category,log_odds,group = id, colour = id)) +
+  geom_line() +
+  guides(colour = 'none') +
+  theme_few()
+
 # -- graph plots -- #
 
 p0
@@ -154,3 +192,17 @@ p3 + p4 + plot_annotation(title = 'hotelban/hotelben')
 ggsave('vis/baseline_2.pdf', width = 12, height = 6)
 p5 + p6 + p7 + plot_annotation(title = 'cselekszenek/cselekednek')
 ggsave('vis/baseline_3.pdf', width = 18, height = 6)
+
+# -- write list -- #
+
+rg = r %>% 
+  distinct(word,carrier_sentence,target_sentence,target1,target2) %>% 
+  rename(base = word) %>% 
+  left_join(r2) %>% 
+  arrange(log_odds) %>% 
+  mutate(p = plogis(log_odds))
+
+filter(rg, variation == "hotelban/hotelben") %>% write_sheet(ss = 'https://docs.google.com/spreadsheets/d/1h7s3IS-BySZeGCTgd1rkzWntgrXuyKylUXlLyg-nTug/edit?usp=sharing', sheet = "hotelban/hotelben")
+filter(rg, variation == "lakom/lakok") %>% write_sheet(ss = 'https://docs.google.com/spreadsheets/d/1h7s3IS-BySZeGCTgd1rkzWntgrXuyKylUXlLyg-nTug/edit?usp=sharing', sheet = "lakom/lakok")
+filter(rg, variation == "cselekszenek/cselekednek") %>% write_sheet(ss = 'https://docs.google.com/spreadsheets/d/1h7s3IS-BySZeGCTgd1rkzWntgrXuyKylUXlLyg-nTug/edit?usp=sharing', sheet = "cselekszenek/cselekednek")
+
