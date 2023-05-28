c99f3aa62319cb1765d342853568ed8bdbb5865f
Peter Racz <pracz@cogsci.bme.hu>
2023-05-28 21:40:20 +0200

    no models

--- models/learner/baseline/fasttext/shell_script_hotelban_3.sh
+++
@@ -1,4 +0,0 @@
-./fasttext supervised -input train_hotelban_3.txt -output model_hotelban_3 -autotune-validation valid_hotelban_3.txt
-./fasttext predict-prob model_hotelban_3.bin test_hotelban_3.txt > pred_hotelban_3.txt
-./fasttext dump model_hotelban_3.bin args > args_hotelban_3.txt
-rm model_hotelban_3.bin
